2ed571460e25afba1d06a8a1eebb1ce945d57435
Eduardo <eduardo@telecajas.com>
2026-03-09 13:39:27 +0100

    Reduce Gunicorn worker count from 2 to 1

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec gunicorn gsc_server_remote:app -w 2 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:${PORT:-8000}
+exec gunicorn gsc_server_remote:app -w 1 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:${PORT:-8000}
